f2a6bce5f0a4f2327ad9216f7583d4fe81c64976
stenioas <stenioas@gmail.com>
2021-07-22 15:07:59 -0300

    feat(home): page home component created

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header";
+import Newsletter from "../../components/Newsletter";
+import Footer from "../../components/Footer/Footer";
+
+// import styles from "./Home.module.scss";
+
+export default function Home() {
+	return (
+		<>
+			<Header />
+			<Newsletter />
+			<Footer />
+		</>
+	);
+}
