9b6b6df5b7d64cdc21d26e0b39df34a7294c7b03
stenioas <stenioas@gmail.com>
2021-07-23 01:49:55 -0300

    feat(home): slideshow and showcase components added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,6 @@
 import Header from "../../components/Header";
+import Slideshow from "../../components/Slideshow";
+import Showcase from "../../components/Showcase";
 import Newsletter from "../../components/Newsletter";
 import Footer from "../../components/Footer/Footer";
 
@@ -8,6 +10,8 @@ export default function Home() {
 	return (
 		<>
 			<Header />
+			<Slideshow />
+			<Showcase />
 			<Newsletter />
 			<Footer />
 		</>
