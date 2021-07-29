112dd8a3557620cc6221e61e1ecfd42efd673827
stenioas <stenioas@gmail.com>
2021-07-29 09:48:33 -0300

    refactor(api): axios implemented and code cleanup

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,34 +1,7 @@
-const postUrl: string = "https://corebiz-test.herokuapp.com/api/v1/newsletter";
-const getUrl: string = "https://corebiz-test.herokuapp.com/api/v1/products";
+import axios from "axios";
 
-export function getProducts() {
-	return fetch(getUrl)
-		.then((response) => {
-			return response.json();
-		})
-		.catch((err) => {
-			console.error(err);
-		});
-}
+const api = axios.create({
+	baseURL: "https://corebiz-test.herokuapp.com/api/v1",
+});
 
-// Teste de funcionalidade não concluido
-export function registerUser(name: string, email: string) {
-	fetch(postUrl, {
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		method: "POST",
-		body: JSON.stringify({
-			email: email,
-			name: name,
-		}),
-	})
-		.then((res) => {
-			console.log(res);
-			alert(res);
-		})
-		.catch((err) => {
-			console.error(err);
-		});
-}
+export default api;
