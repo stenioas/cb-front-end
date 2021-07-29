edd28ac212d9049dcc2213378de6e070a2d603f5
stenioas <stenioas@gmail.com>
2021-07-29 14:03:21 -0300

    refactor(newsletter): form validate and post method implemented

--- src/components/Newsletter/Newsletter.tsx
+++ src/components/Newsletter/Newsletter.tsx
@@ -1,48 +1,88 @@
-// PENDENTE
-// Implementar validações de inputs com o hook useForm
-// Implementar mensagens de erro para o usuário
-// Implementar mensagem após o cadastro com sucesso
+import { useState, FormEvent } from "react";
 
-import { useState } from "react";
-
-import { registerUser } from "../../services/api";
+import useForm from "../../hooks/useForm";
+import api from "../../services/api";
 
 import styles from "./Newsletter.module.scss";
 
 export default function Newsletter() {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
+	const useremail = useForm("email");
+	const username = useForm("username");
+	const [hasRegistered, setHasRegistered] = useState(false);
+
+	const newRegister = async (event: FormEvent) => {
+		event.preventDefault();
+		if (useremail.validate() && username.validate()) {
+			await api.post("newsletter", {
+				email: useremail.value,
+				name: username.value,
+			});
+		}
+
+		setHasRegistered(true);
+	};
+
+	const handleRegister = () => {
+		if (hasRegistered) {
+			setHasRegistered(false);
+			username.setvalue("");
+			useremail.setvalue("");
+		}
+	};
 
 	return (
 		<section className={styles.container}>
 			<div className={styles.wrapper}>
-				<p className={styles.title}>
-					Participe de nossas news com promoções e novidades!
-				</p>
-				<form
-					className={styles.form}
-					onSubmit={() => registerUser(name, email)} // Teste de funcionalidade não concluido
-				>
-					<label>
-						<input
-							type="text"
-							placeholder="Digite seu nome"
-							value={name}
-							required
-							onChange={(e) => setName(e.target.value)}
-						/>
-					</label>
-					<label>
-						<input
-							type="email"
-							placeholder="Digite seu email"
-							value={email}
-							required
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</label>
-					<button type="submit">Eu quero!</button>
-				</form>
+				{!hasRegistered ? (
+					<>
+						<p className={styles.title}>
+							Participe de nossas news com promoções e novidades!
+						</p>
+						<form className={styles.form} onSubmit={newRegister} noValidate>
+							<label>
+								<input
+									type="text"
+									placeholder="Digite seu nome"
+									required
+									{...username}
+								/>
+								{username.error && (
+									<span className={styles.error}>
+										Preencha o seu nome completo
+									</span>
+								)}
+							</label>
+							<label>
+								<input
+									type="email"
+									placeholder="Digite seu email"
+									required
+									{...useremail}
+								/>
+								{useremail.error && (
+									<span className={styles.error}>
+										Preencha com um e-mail válido
+									</span>
+								)}
+							</label>
+							{username.error || useremail.error ? (
+								<button type="button">Eu quero!</button>
+							) : (
+								<button type="submit">Eu quero!</button>
+							)}
+						</form>
+					</>
+				) : (
+					<div className={styles.registered}>
+						<h3>Seu e-mail foi cadastrado com sucesso!</h3>
+						<p>
+							A partir de agora você receberá as novidades e ofertas exclusivas.
+						</p>
+						<button type="button" onClick={handleRegister}>
+							Cadastrar novo e-mail
+						</button>
+					</div>
+				)}
 			</div>
 		</section>
 	);
