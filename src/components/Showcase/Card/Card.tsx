bfc8c9c0fd9b2c56f45138fa350b5890344ce480
stenioas <stenioas@gmail.com>
2021-07-29 14:04:31 -0300

    refactor(showcase): get data from api and card styles improvements

--- src/components/Showcase/Card/Card.tsx
+++ src/components/Showcase/Card/Card.tsx
@@ -1,42 +1,55 @@
-// PENDENTE
-// Implementar campos faltantes do produto
-// Implementar tarja de produto em oferta
-// Implementar estilos do card
+// IMPLEMENTAR
+// Tarja de produto em oferta
 
-import useCart from "../../../hooks/useCart";
+import Rating from "react-star-rating-component";
 
+import useStore from "../../../hooks/useStore";
+import { Product } from "../../../utils/types";
 import styles from "./Card.module.scss";
 
-type InstallmentsType = {
-	quantity: number;
-	value: number;
-};
-
-interface CardProps {
-	productId: number;
-	productName: string;
-	stars: number;
-	imageUrl: string;
-	listPrice: number | null;
-	price: number;
-	installments: Array<InstallmentsType> | Array<null>;
-}
-
-export default function Card(props: CardProps) {
-	const { cartCounter, setCartCounter } = useCart();
+export default function Card(props: Product) {
+	const { cartCounter, setCartCounter } = useStore();
 
 	return (
 		<div className={styles.card}>
 			<img src={props.imageUrl} alt="Produto" />
-			<h4>{props.productName}</h4>
-			<div className={styles.price}>
-				<p className={styles.listPrice}>R$ {props.listPrice}</p>
-				<p className={styles.price}>R$ {props.price}</p>
-				<p className={styles.installments}>{props.installments}</p>
+			<div className={styles.details}>
+				<h4>{props.productName}</h4>
+				<Rating
+					name="rate"
+					starCount={5}
+					value={props.stars}
+					starColor="#F8475F"
+				/>
+				{props.listPrice && (
+					<p className={styles.listPrice}>
+						de
+						{new Intl.NumberFormat("pt-BR", {
+							style: "currency",
+							currency: "BRL",
+						}).format(props.listPrice / 100)}
+					</p>
+				)}
+				<p className={styles.price}>
+					por{" "}
+					{new Intl.NumberFormat("pt-BR", {
+						style: "currency",
+						currency: "BRL",
+					}).format(props.price / 100)}
+				</p>
+				{props.installments.length > 0 && (
+					<p className={styles.installments}>
+						ou em {props.installments[0]?.quantity}x de{" "}
+						{new Intl.NumberFormat("pt-BR", {
+							style: "currency",
+							currency: "BRL",
+						}).format(props.installments[0]?.value / 100)}
+					</p>
+				)}
+				<button type="button" onClick={() => setCartCounter(cartCounter + 1)}>
+					COMPRAR
+				</button>
 			</div>
-			<button type="button" onClick={() => setCartCounter(cartCounter + 1)}>
-				COMPRAR
-			</button>
 		</div>
 	);
 }
--- src/components/Showcase/Showcase.tsx
+++ src/components/Showcase/Showcase.tsx
@@ -1,16 +1,13 @@
-// PENDENTE
-// Implementar construção da vitrine consumindo a api fornecida
-// Implementar carousel para a lista de produtos
+// IMPLEMENTAR
+// Carousel para a lista de produtos
 
+import useStore from "../../hooks/useStore";
 import Card from "./Card";
-import { getProducts } from "../../services/api";
 
 import styles from "./Showcase.module.scss";
 
 export default function Showcase() {
-	const products = getProducts();
-	// Teste de retorno da promise
-	console.log(products);
+	const { productsList } = useStore();
 
 	return (
 		<section className={styles.container}>
@@ -18,42 +15,22 @@ export default function Showcase() {
 				<h2 className={styles.title}>Mais Vendidos</h2>
 				<div className={styles.line} />
 				<div className={styles.products}>
-					<Card
-						productId={1}
-						productName="SAPATO FLOATER PRETO"
-						stars={5}
-						imageUrl="https://corebiz-test.herokuapp.com/images/product-1.png"
-						listPrice={29900}
-						price={39900}
-						installments={[]}
-					/>
-					<Card
-						productId={2}
-						productName="SAPATO FLOATER PRETO"
-						stars={5}
-						imageUrl="https://corebiz-test.herokuapp.com/images/product-2.png"
-						listPrice={29900}
-						price={39900}
-						installments={[]}
-					/>
-					<Card
-						productId={3}
-						productName="SAPATO FLOATER PRETO"
-						stars={5}
-						imageUrl="https://corebiz-test.herokuapp.com/images/product-3.png"
-						listPrice={29900}
-						price={39900}
-						installments={[]}
-					/>
-					<Card
-						productId={1}
-						productName="SAPATO FLOATER PRETO"
-						stars={5}
-						imageUrl="https://corebiz-test.herokuapp.com/images/product-4.png"
-						listPrice={29900}
-						price={39900}
-						installments={[]}
-					/>
+					{productsList
+						? productsList.map((i) => {
+								return (
+									<Card
+										key={i.productId}
+										productId={i.productId}
+										productName={i.productName}
+										stars={i.stars}
+										imageUrl={i.imageUrl}
+										listPrice={i.listPrice}
+										price={i.price}
+										installments={i.installments}
+									/>
+								);
+						  })
+						: "Nenhum produto encontrado!"}
 				</div>
 			</div>
 		</section>
